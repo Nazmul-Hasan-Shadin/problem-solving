6f4091bee00121b26a0c376a1bae7c7dd1d7686b
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2025-01-06 22:17:41 +0600

    problem solving has initilaized

---
+++ arrayFiltering.ts
@@ -0,0 +1,14 @@
+/*1. Task: Array Filtering And Mapping
+
+Create An Array Of Objects, Each Representing A Person With Properties Like Name, Age, And Gender. Write A Function To Filter Out All Females And Then Map The Remaining People To An Array Of Names. Print The Final Result.
+
+*/
+
+const peoples = {
+  people: [
+    { name: "John", age: 25, gender: "Male" },
+    { name: "Jane", age: 22, gender: "Female" },
+    { name: "Mike", age: 30, gender: "Male" },
+    { name: "Emily", age: 27, gender: "Female" },
+  ],
+};
